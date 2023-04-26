1e2f77a1e4607fd5568e8d9a66f6fa92611e99af
Jan Phillip Kretzschmar <jan.kretzschmar@valtech.com>
2023-04-26 14:52:51 +0200

    feat: configure maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
+    `java-library`
     kotlin("jvm") version "1.7.10"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    `maven-publish`
+    signing
 }
-
-group = "com.valtechmobility.gradle.credentials.onepassword"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -27,9 +28,52 @@ tasks.withType<Test> {
 
 dependencies {
     implementation(gradleApi())
-    implementation(localGroovy())
 
     testImplementation("junit:junit:4.13.2")
     testImplementation(gradleTestKit())
     testImplementation("io.mockk:mockk:1.12.3")
+}
+
+group = "com.valtechmobility"
+version = "0.0.1"
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            artifactId = "gradle-credentials-onepassword"
+            from(components["java"])
+                    versionMapping {
+                        usage("java-api") {
+                            fromResolutionOf("runtimeClasspath")
+                        }
+                        usage("java-runtime") {
+                            fromResolutionResult()
+                        }
+                    }
+            pom {
+                name.set("gradle-credentials-onepassword")
+                description.set("gradle-credentials-onepassword is a gradle repository credential integration for 1Password")
+                url.set("https://github.com/ValtechMobility/gradle-onepassword-credentials-plugin")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("http://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("janphkre")
+                        name.set("Jan Phillip Kretzschmar")
+                        email.set("janphkre@gmx.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git://github.com/ValtechMobility/gradle-onepassword-credentials-plugin.git")
+                    developerConnection.set("scm:git://github.com/ValtechMobility/gradle-onepassword-credentials-plugin.git")
+                    url.set("https://github.com/ValtechMobility/gradle-onepassword-credentials-plugin")
+                }
+            }
+        }
+    }
 }
